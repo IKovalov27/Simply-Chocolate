7aa590c86beb92188ed74b0b21625bc9728ce59a
Illia Kovalov <kovalev.illia2009@gmail.com>
2024-03-15 21:29:30 +0100

    17

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import htmlImages from 'vite-plugin-html-images'
 
 export default defineConfig({
   root: 'src',
@@ -12,5 +11,5 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html']), htmlImages()],
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
